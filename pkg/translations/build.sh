b760709b169a0722f34faf543b2e85cfd7b05af2
David Newhall II <david@sleepers.pro>
2023-03-24 18:08:45 -0700

    only update English

--- pkg/translations/build.sh
+++ pkg/translations/build.sh
@@ -3,13 +3,9 @@
 # Builds the translation json files and renames them into place.
 # Add a new language by creating a folder for it: mkdir locales/xx
 
-PKG="golang.org/x/text/cmd/gotext@latest"
+RUN="golang.org/x/text/cmd/gotext@latest"
 LANG=$(ls locales|xargs|tr ' ' ,)
+PKGS="github.com/Notifiarr/toolbarr/pkg/..."
 
-go run ${PKG} -srclang=en update \
-    -out=catalog.go \
-    -lang=${LANG} \
-    github.com/Notifiarr/toolbarr/pkg/... && \
-    for file in $(ls locales/*/out.gotext.json); do 
-        mv "${file}" "$(dirname "${file}")/messages.gotext.json"
-    done
+go run ${RUN} -srclang=en update -out=catalog.go -lang=${LANG} ${PKGS} && \
+    mv locales/en/out.gotext.json locales/en/messages.gotext.json
