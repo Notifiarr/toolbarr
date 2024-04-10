194c49172c8b34a27e887b62d8a2e511bd66b851
David Newhall II <david@sleepers.pro>
2024-04-10 11:55:23 -0700

    we arrived

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,9 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
+  compilerOptions: {
+    customElement: true,
+  },
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
